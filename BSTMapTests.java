a61a322b42a741f2fae01d1ae0acecb5609d0b5b
Derek Hessinger <derekjhessinger@gmail.com>
2022-10-28 15:11:45 -0400

    updates

--- BSTMapTests.java
+++ BSTMapTests.java
@@ -26,7 +26,7 @@ public class BSTMapTests{
 			System.out.println("");
 		}
 
-		// case 2: testing get() ***TODO: Finish this test***
+		// case 2: testing put() ***TODO: Finish this test***
 		{
 			System.out.println("TESTING: put()");
 
@@ -78,5 +78,84 @@ public class BSTMapTests{
 			System.out.println("Testing complete!");
 			System.out.println("");
 		}
+
+		// case 4: testing clear()
+		{
+			System.out.println("TESTING: clear()");
+
+			// setup
+			BSTMap<String, Integer> map1 = new BSTMap<String, Integer>();
+			map1.put("first", 1);
+			map1.put("second", 2);
+			map1.put("third", 3);
+			map1.clear();
+
+			// verify
+			System.out.println(map1 + " == ");
+
+			// test
+			assert map1.size() == 0 : "Error in BSTMap::clear()";
+
+			System.out.println("Testing complete!");
+			System.out.println("");
+		}
+
+		// case 5: testing get()
+		{
+			System.out.println("TESTING: get()");
+
+			// setup
+			BSTMap<String, Integer> map1 = new BSTMap<String, Integer>();
+			map1.put("first", 1);
+
+			// verify
+			System.out.println(map1.get("first") + " == 1");
+			System.out.println(map1.get("second") + " == null");
+
+			// test
+			assert map1.get("first") == 1 : "Error in BSTMap::get()";
+			assert map1.get("second") == null : "Error in BSTMap::get()";
+
+			System.out.println("Testing complete!");
+			System.out.println("");
+		}
+
+		// case 6: testing contains()
+		{
+			System.out.println("TESTING: contains()");
+
+			// setup
+			BSTMap<String, Integer> map1 = new BSTMap<String, Integer>();
+			map1.put("first", 1);
+
+			// verify
+			System.out.println(map1.containsKey("first") + " == true");
+			System.out.println(map1.containsKey("second") + " == false");
+
+			// test
+			assert map1.containsKey("first") == true : "Error in BSTMap::contains()";
+			assert map1.containsKey("second") == false : "Error in BSTMap::contains()";
+
+			System.out.println("Testing complete!");
+			System.out.println("");
+		}
+
+		// case 7: testing keySet() ***TODO: Finish this test***
+		{
+			System.out.println("TESTING: keySet()");
+
+			// setup
+			BSTMap<String, Integer> map1 = new BSTMap<String, Integer>();
+			map1.put("first", 1);
+			map1.put("second", 2);
+			map1.put("third", 3);
+			// verify
+			System.out.println(map1.keySet() + " == [first, second, third]");
+
+			// test
+			assert map1.keySet() ==  : "Error in BSTMap::keySet()";
+		}
+
+		
 	}
 }
